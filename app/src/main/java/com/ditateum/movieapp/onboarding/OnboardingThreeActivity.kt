9516b8bbeebee7b96d36b5c4e8b2bacac281fb2c
Dita Permata Putra <ditateum@gmail.com>
2022-05-28 11:49:46 +0700

    movie-app-android-native-kotlin continue project

---
+++ app/src/main/java/com/ditateum/movieapp/onboarding/OnboardingThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.ditateum.movieapp.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ditateum.movieapp.R
+
+class OnboardingThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_three)
+    }
+}
