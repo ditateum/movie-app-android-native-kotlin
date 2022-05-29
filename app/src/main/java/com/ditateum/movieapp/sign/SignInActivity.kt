b448923a451a5596c2b81e482f79285a2147bb30
Dita Permata Putra <ditateum@gmail.com>
2022-05-29 12:58:01 +0700

    setup firebase with android studio

--- app/src/main/java/com/ditateum/movieapp/sign/SignInActivity.kt
+++ app/src/main/java/com/ditateum/movieapp/sign/SignInActivity.kt
@@ -1,12 +1,27 @@
 package com.ditateum.movieapp.sign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ditateum.movieapp.R
+import androidx.appcompat.app.AppCompatActivity
+import com.ditateum.movieapp.databinding.ActivitySignInBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener {
+            // Write a message to the database
+            val database = Firebase.database
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
+        }
+
     }
 }
